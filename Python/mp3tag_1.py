faa8a2e0f35a1c51886957c735317cead3094509
peleidmju <edpshenicin@yandex.ru>
2023-10-07 20:44:23 +0300

    csv и 2c почему то не до конца

--- Python/mp3tag_1.py
+++ Python/mp3tag_1.py
@@ -9,5 +9,12 @@ filez = glob.glob('E:\English\PimsleurNew\Second\mp3\Pimsleur_II_02-??.mp3')
 for file in filez:
     str_list = file.split('-')
     shutil.move(file, '-0'.join(str_list))
+# filez = glob.glob('E:\English\PimsleurNew\Second\mp3\Pimsleur_II_02-*.mp3')
+filez = glob.glob('E:\English\Python\\tempfor\Pimsleur_II_02-*.mp3')
+for file in filez:
+    str_list_1 = file.split('_', 1)
+    str_list_2 = str_list_1[1].split('-')
+    shutil.move(file, str_list_1[0] + '_32-' + str_list_2[1])
+    # print(str_list_1[0] + '_32-' + str_list_2[1])
 print(len(filez))
-print(filez)
+# print(filez)
--- Python/mp3tag_2.py
+++ Python/mp3tag_2.py
@@ -6,9 +6,26 @@ from mutagen.id3 import ID3
 import os
 import shutil
 import glob
+import csv
 
-# Работаем с тегами файлов mp3
-filez = glob.glob('E:\English\Python\\tempfor\Pimsleur_II_02-*.mp3')
-print(len(filez))
-mp3file = MP3(filez[1], ID3=EasyID3)
+# Работаем с тегами файлов mp3, уже разбитыми на
+# маленькие звуковые файлы.
+filez = glob.glob('E:\English\Python\\tempfor\Pimsleur_32-*.mp3')
+count_filez = len(filez)
+str_count_filez = str(count_filez)
+mp3file = MP3(filez[124], ID3=EasyID3)
 print(mp3file)
+csv_file_list = []
+with open('E:\English\Python\\tempfor\Pimsleur_text_2_02.csv', encoding='utf-8') as file:
+    file_csv_reader = csv.DictReader(file)
+    for line in file_csv_reader:
+        csv_file_list.append(line)
+print(csv_file_list)
+for i in range(count_filez):
+    tracknumber = str(i+1).zfill(len(str_count_filez)) + '/' + str_count_filez
+    mp3file = MP3(filez[i], ID3=EasyID3)
+    mp3file['album'] = 'Lesson 32'
+    mp3file['artist'] = 'Pimsler 2'
+    mp3file['genre'] = 'English'
+    mp3file['tracknumber'] = tracknumber
+    mp3file.save()
