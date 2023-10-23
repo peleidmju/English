18e3ec4b4de51c60601f8f3588b69060fe89edaf
peleidmju <edpshenicin@yandex.ru>
2023-10-23 17:21:50 +0300

    правильно переименовал

--- Python/Pim_1/divide_txtfiles_1.py
+++ Python/Pim_1/divide_txtfiles_1.py
@@ -3,10 +3,10 @@ import shutil
 
 
 def rename_mp3():
-    files = glob.glob("E:\English\PimsleurNew\Second\mp3\Pimsleur_II_05-*.mp3")
+    files = glob.glob("E:\English\PimsleurNew\Second\mp3\Pimsleur_35_*.mp3")
     for file in files:
-        new_name = ((file.split('-')[1]).split('.')[0]).zfill(3)
-        new_name = file.split('_')[0] + '_35_' + new_name + '.mp3'
+        new_name = ((file.split('_')[2]).split('.')[0]).zfill(3)
+        new_name = file.split('_')[0] + '_35-' + new_name + '.mp3'
         shutil.move(file, new_name)
 
 
